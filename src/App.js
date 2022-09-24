c05cb8f78b3f739fa526cc3d55e12cad252c14c1
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-24 22:39:34 +0100

    File and folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <div className='App'></div>;
-}
+import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ChannelDetail from './components/ChannelDetail';
+import Feed from './components/Feed';
+import Navbar from './components/Navbar';
+import SearchFeed from './components/SearchFeed';
+import VideoDetail from './components/VideoDetail';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:id' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
