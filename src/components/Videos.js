d8da15effef7a6e0ce4318c00ca0edb5c20186b7
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-27 23:04:53 +0100

    Added Dynamic Direction

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -3,9 +3,15 @@ import React from 'react';
 import ChannelCard from './ChannelCard';
 import VideoCard from './VideoCard';
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return 'Loading...';
   return (
-    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+    <Stack
+      direction={direction || 'row'}
+      flexWrap='wrap'
+      justifyContent='start'
+      gap={2}
+    >
       {videos.map((item, idx) => {
         return (
           <Box key={idx}>
